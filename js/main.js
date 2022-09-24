16bb71c73f009944515186d33a9d797dc774449f
Stalmax76 <StelmakhV76@gmail.com>
2022-09-24 23:09:02 +0200

    added scss and css files

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import sum from './sum.mjs'
+
+
+
+
+
+
+
+const myHeading = document.querySelector('h1');
+console.log(myHeading);
+myHeading.textContent = 'Hello world!';
+console.log(myHeading);
+//const value1 = 45
+//const value2 = 23
+//const str = 'hello'
+//const b = 'true'
+
+
+
+//console.log('======================================================================')
+//const res = sum(value1, value2)
+//console.log(res)
+//const res1 = sum(value1, str)
+//console.log(res1)
+//const res2 = sum(value2, b)
+//console.log(res2)
+//console.log('======================================================================')
+
+
+
